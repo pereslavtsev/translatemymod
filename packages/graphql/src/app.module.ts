3d0e7f9b692a98d41ca9b77a1ec168ab30016803
Vitaly Pereslavtsev <dev@pereslavtsev.com>
2022-11-21 18:06:56 +0300

    init backend

--- packages/graphql/src/app.module.ts
+++ packages/graphql/src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { ProjectsModule } from './projects/projects.module';
+import { ResourcesModule } from './resources/resources.module';
+import { LocalisationModule } from './localisation/localisation.module';
+import * as path from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: path.join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+    }),
+    ProjectsModule,
+    ResourcesModule,
+    LocalisationModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
